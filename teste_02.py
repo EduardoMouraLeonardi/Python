ba7293910c261802871bb79b4bb8f109ed053ea3
Eduardo Moura Leonardi <eduardo.moura.leonardi@gmail.com>
2023-05-09 17:08:16 -0300

    Atualização do arquivo

--- teste_02.py
+++ teste_02.py
@@ -6,22 +6,27 @@ nome, sobrenome, idade = "eduardo", "MOURA", "20"
 # Exibindo o Tamanho de um Dado do Tipo STRING. #
 print("O tamanho do dado na variavel nome é: ")
 print(len(nome))
+print("\n")
 
 # Exibindo a Posição de um Caractere Expecifico em uma STRING. #
 print("Na variavel \"nome\" a letra \"e\" se encontra na posição: ")
 print(nome.find("e"))# "e" é representado como 0 pois computadores começão contagens a partir de 0 ao inves de 1. #
+print("\n")
 
 # Contar Quantos Caracteres Especificos Estão Presentes no Dado Armazanado. #
 print("Na variavel \"nome\" a letra \"d\" esta presente: ")
 print(str(nome.count("d"))+" veses")
+print("\n")
 
 # Alterando Caracteres Especificos Presentes no Dado Armazanado. #
 print("Ao alterar caracteres especificos da variavel \"nome\" as letras \"d\" se tornam \"D\"")
 print("De "+nome+" para "+nome.replace("d","D"))
+print("\n")
 
 # Transformando a 1° Letra de uma STRING em Caixa Alta. #
 print("Capitalizando a 1° letra da variavel \"nome\"")
 print(nome.capitalize())
+print("\n")
 
 # Transformando Todo valor de uma STRING em Caixa Alta. #
 print("Capitalizando toda a variavel \"nome\"")
